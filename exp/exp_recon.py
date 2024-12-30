81f5b3f2692219a7b5f88d14f0f31f9eed503d8e
Daniel-Lin-S <75996224+Daniel-Lin-S@users.noreply.github.com>
2024-12-30 14:26:12 +0800

    Refactored VAE experiment to be more flexible

--- exp/exp_recon.py
+++ exp/exp_recon.py
@@ -7,19 +7,23 @@ import time
 from math import gcd
 from typing import List, Optional
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 from data_provider.data_factory import data_provider
-from utils.tools import EarlyStopping, adjust_learning_rate, plot_reconstruction_for_channels
+from utils.tools import (
+    EarlyStopping, adjust_learning_rate, plot_reconstruction_for_channels
+)
+from utils.logger import Logger
 from models.VAE2D import VAE2D
 from exp.exp_basic import Exp_Basic
 
 
-class Exp_VAE2D(Exp_Basic):
+class Exp_Recon(Exp_Basic):
     """
     Experiment class for training and testing the VAE2D model
     for reconstruction tasks.
     """
-    def __init__(self, args, config):
+    def __init__(self, args, config, logger: Logger):
         """
         Initialize the VAE2D experiment.
 
@@ -30,45 +34,59 @@ class Exp_VAE2D(Exp_Basic):
             - model: Name of the model (string)
             - seq_len: Input sequence length
             - enc_in: Number of input channels
-            - n_fft: Number of FFT points
             - train_epochs: Number of training epochs
             - learning_rate: Learning rate for optimizer
             - patience: Early stopping patience
             - checkpoints: Path to save checkpoints
-            - vae: Dictionary of VAE-specific hyperparameters
-            - exp_params: Experiment parameters including beta for KL divergence
-            - output_path: Path to save outputs
         config : dict
-            Configuration file for initializing VAE2D model.
+            Configuration file for initializing the model.
+        logger : DualLogger
+            the logger used to store messages into log file
+            and print messages into console.
         """
         self.config = config
-        self.init_beta = config['vae']['beta_init']
-        self.final_beta = config['vae']['beta']
-        super(Exp_VAE2D, self).__init__(args)
+        self.logger = logger
+        super(Exp_Recon, self).__init__(args)
 
-    def _build_model(self):
+    def _build_model(self) -> nn.Module:
         """
-        Build the VAE2D model.
+        Build the model.
 
         Returns
         -------
-        VAE2D
-            Initialised VAE2D model.
-        """
-        recon_len = gcd(self.args.seq_len, self.args.pred_len)
-        self.recon_len = recon_len
-        print(f'Reconstruction length: {recon_len}')
-        model = VAE2D(
-            in_channels=self.args.enc_in,
-            input_length=recon_len,
-            config=self.config
-        )
+        torch.nn.Module
+            Initialised model.
 
-        total_params = sum(p.numel() 
-                           for p in model.parameters() 
-                           if p.requires_grad)
-        print('Number of trainable parameters of the '
-              f'model {self.args.model}: {total_params}')
+        Notes
+        -----
+        model initalisation must take two arguments:
+        args, config. Where args is a Namespace
+        with attributes recon_len (input length)
+        and enc_in (number of input channels)
+        and other training parameters.
+        config is a dictionary taken from
+        a yaml file consisting of the
+        model-specific arguments.
+        """
+        model_dict = {
+            'VAE' : VAE2D
+        }
+
+        args = self.args
+        if args.model not in model_dict:
+            self.logger.log(
+                f"Invalid model name '{args.model}'. Available models are: "
+                + ", ".join(model_dict.keys()),
+                level='error'
+            )
+            raise
+
+        recon_len = gcd(self.args.seq_len, self.args.pred_len)
+        args.recon_len = recon_len
+        self.logger.log(f'Reconstruction length: {recon_len}', level='debug')
+
+        model = model_dict[args.model](args, self.config)
+        self._print_model(model, self.args.model)
 
         if self.args.use_multi_gpu and self.args.use_gpu:
             model = nn.DataParallel(
@@ -89,7 +107,9 @@ class Exp_VAE2D(Exp_Basic):
         -------
         dataset, DataLoader
         """
-        dataset, dataloader = data_provider(self.args, flag, 'recon')
+        dataset, dataloader = data_provider(
+            self.args, flag, mode='recon',
+            logger=self.logger)
         return dataset, dataloader
 
     def _select_optimizer(self):
@@ -106,20 +126,35 @@ class Exp_VAE2D(Exp_Basic):
         scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
         return optimizer, scheduler
 
-    def adjusted_beta(self, epoch: int, total_epochs: int) -> float:
+    def _configure_epoch_args(self, epoch: int):
         """
-        Gradual annealing schedule for the KL loss.
-        Starts with a lower beta value and increases it gradually.
+        Configure model-specific arguments
+        for each training epoch.
 
-        Return
-        ------
-        float
-            the beta-value for current epoch
+        Parameters
+        ----------
+        epoch : int
+            The current training epoch.
+
+        Returns
+        -------
+        args : dict
+            A dictionary of arguments to pass to the model.
         """
-        return self.init_beta + (
-            self.final_beta - self.init_beta) * (epoch / total_epochs)
+        # Default args, can be expanded based on model type
+        epoch_args = {}
 
-    def train(self, setting):
+        if isinstance(self.model, VAE2D):
+            init_beta = self.config['vae']['beta_init']
+            final_beta = self.config['vae']['beta']
+            beta = init_beta + (final_beta - init_beta) * (
+                epoch / self.args.train_epochs)
+            epoch_args['beta'] = beta
+        
+        return epoch_args
+
+    def train(self, setting: str,
+              log_interval: int=50):
         """
         Train the model.
 
@@ -127,6 +162,9 @@ class Exp_VAE2D(Exp_Basic):
         ----------
         setting : str
             A unique identifier for the training run.
+        log_interval : int
+            Number of batches between which the
+            batch loss should be printed.
 
         Returns
         -------
@@ -141,7 +179,8 @@ class Exp_VAE2D(Exp_Basic):
             os.makedirs(path)
 
         train_steps = len(train_loader)
-        early_stopping = EarlyStopping(patience=self.args.patience, verbose=True)
+        early_stopping = EarlyStopping(
+            patience=self.args.patience, verbose=True, logger=self.logger)
 
         model_optim, scheduler = self._select_optimizer()
 
@@ -149,60 +188,83 @@ class Exp_VAE2D(Exp_Basic):
             iter_count = 0
             train_loss = []
             self.model.train()
+            epoch_args = self._configure_epoch_args(epoch)
             epoch_time = time.time()
 
-            beta = self.adjusted_beta(epoch, self.args.train_epochs)
-
-            for i, batch in enumerate(train_loader):
-                batch_x, _ = batch  # Assuming batch contains only data and no labels
+            # build progress bar
+            train_bar = tqdm(train_loader,
+                             desc=f"Epoch {epoch} [Training]",
+                             leave=False)
+            for i, (batch_x, _) in enumerate(train_bar):
                 batch_x = batch_x.float().to(self.device)
 
                 model_optim.zero_grad()
-                recons_loss, kl_losses = self.model(batch_x, i, beta=beta)
-                loss = recons_loss['LF.time'] + recons_loss['HF.time'] + kl_losses['combined']
+                loss_dict = self.model(batch_x, i, **epoch_args)
+                try:
+                    loss = loss_dict['total']
+                except KeyError:
+                    self.logger.log(
+                        'KeyError: The loss dictionary must include a key "total", '
+                        'indicating the overall loss. '
+                        'Please ensure model.forward returns '
+                        'a properly formatted loss dictionary.',
+                        level="error"
+                    )
+                    raise
 
                 train_loss.append(loss.item())
                 loss.backward()
                 model_optim.step()
 
-                iter_count += 1
-                if (i + 1) % 100 == 0:
-                    print(f"Epoch: {epoch+1}, Step: {i+1}/{train_steps}, "
-                          f"Total loss: {loss.item():.4f}; "
-                          f"Reconstruction losses: LF {recons_loss['LF.time']:.4f}, "
-                          f"HF {recons_loss['HF.time']:.4f}; "
-                          f"KL loss: {kl_losses['combined']:.4f}")
+                train_bar.set_postfix(average_loss=np.mean(train_loss))
 
-            print(f"Epoch: {epoch+1} cost time: {(time.time() - epoch_time):.2f}s")
+                iter_count += 1
+                if (i + 1) % log_interval == 0:
+                    batch_message = f"Epoch: {epoch+1}, " + \
+                        f"Step: {i+1}/{train_steps}, " + \
+                            f"Total Loss: {loss.item():.4f}; "
+                    detailed_message = "Loss breakdown: " + \
+                        f"{', '.join([f'{key}: {value:.4f}'
+                                      for key, value in loss_dict.items()
+                                      if key != 'total'])}"
+                    self.logger.log(batch_message+detailed_message,
+                                    level='debug')
+
+            epoch_duration = time.time() - epoch_time
             train_loss = np.average(train_loss)
 
-            vali_loss = self.validate(
+            vali_loss = self.vali(
                 vali_data, vali_loader, setting, epoch, flag='Vali')
-            test_loss = self.validate(
+            test_loss = self.vali(
                 test_data, test_loader, setting, epoch, flag='Test')
+            test_time = time.time() - epoch_duration - epoch_time
 
-            print(
-                f"Epoch: {epoch+1}, Train Loss: {train_loss:.4f}"
-                f", Vali Loss: {vali_loss:.4f}, Test Loss: {test_loss:.4f}")
+            message = f"Epoch: {epoch+1}, Train Loss: {train_loss:.4f}, " + \
+                f"Vali Loss: {vali_loss:.4f}, Test Loss: {test_loss:.4f}" + \
+                    f", Training Time: {epoch_duration:.2f}s, " + \
+                        f"Testing Time {test_time:.2f}s"
+
+            self.logger.log(message)
             early_stopping(vali_loss, self.model, path)
 
             if early_stopping.early_stop:
-                print("Early stopping")
+                self.logger.log("Early stopping ...")
                 break
 
             scheduler.step()
             # adjust_learning_rate(model_optim, epoch + 1, self.args)
             for param_group in model_optim.param_groups:
-                print(f"Epoch {epoch+1} Learning Rate: {param_group['lr']}")
+                lr_message = f"Learning Rate updated to {param_group['lr']}"
+                self.logger.log(lr_message, level='debug')
 
         # restore the model with lowest validation loss
-        print('Training finished, saving the best model...')
+        self.logger.log('Retrieving the best model ...', level='debug')
         best_model_path = os.path.join(path, 'checkpoint.pth')
         self.model.load_state_dict(torch.load(best_model_path))
-        test_loss = self.validate(
+        test_loss = self.vali(
                 test_data, test_loader, setting, epoch, plot_recon=True,
                 flag='Final test')
-        print(f'Final test loss: {test_loss}')
+        self.logger.log(f'Final test loss: {test_loss}')
         
         folder_path = './test_results/' + setting
         full_data = np.concatenate((train_data.data, vali_data.data, test_data.data),
@@ -213,7 +275,7 @@ class Exp_VAE2D(Exp_Basic):
 
         return self.model
 
-    def validate(self, vali_data, vali_loader, setting, epoch_id,
+    def vali(self, vali_data, vali_loader, setting, epoch_id,
                  plot_recon: bool=False, flag='Vali'):
         """
         Validate the model.
@@ -237,18 +299,17 @@ class Exp_VAE2D(Exp_Basic):
         """
         self.model.eval()
         total_loss = []
-        beta = self.adjusted_beta(epoch_id, self.args.train_epochs)
+        epoch_args = self._configure_epoch_args(epoch_id)
 
         folder_path = './test_results/' + setting
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
 
         with torch.no_grad():
-            recon_losses_lf = []
-            recon_losses_hf = []
-            recon_losses_kl = []
-            for i, batch in enumerate(vali_loader):
-                batch_x, _ = batch
+            vali_bar = tqdm(vali_loader,
+                            desc=f"Epoch {epoch_id} [Validation]",
+                            leave=False)
+            for i, (batch_x, _) in enumerate(vali_bar):
                 batch_x = batch_x.float().to(self.device)
 
                 if i % 20 == 0 and plot_recon:
@@ -256,20 +317,17 @@ class Exp_VAE2D(Exp_Basic):
                 else:
                     save_recon = False
 
-                recons_loss, kl_losses = self.model(
+                loss_dict = self.model(
                     batch_x, i, save_recon=save_recon,
-                    folder_path=folder_path, beta=beta)
-                recon_losses_lf.append(recons_loss['LF.time'].item())
-                recon_losses_hf.append(recons_loss['HF.time'].item())
-                recon_losses_kl.append(kl_losses['combined'].item())
+                    folder_path=folder_path, **epoch_args)
+                
+                total_loss.append(loss_dict['total'].item())
 
-                loss = recons_loss['LF.time'] + recons_loss['HF.time'] + kl_losses['combined']
-                total_loss.append(loss.item())
-
-            print(f"{flag} loss breakdown -- "
-                  f"Reconstruction losses: LF {np.average(recon_losses_lf)}"
-                  f", HF {np.average(recon_losses_hf)}; "
-                  f"KL loss: {np.average(recon_losses_kl)}")
+            loss_message = f"{flag} Loss breakdown: " + \
+                f"{', '.join([f'{key}: {value:.4f}'
+                              for key, value in loss_dict.items()
+                              if key != 'total'])}"
+            self.logger.log(loss_message, level='debug')
 
         total_loss = np.average(total_loss)
         self.model.train()
@@ -298,7 +356,7 @@ class Exp_VAE2D(Exp_Basic):
 
         Notes
         -----
-        If length of data is not divisible by self.recon_len,
+        If length of data is not divisible by self.args.recon_len,
         the last section will be cut off and ignored.
         """
         self.model.eval()
@@ -307,8 +365,8 @@ class Exp_VAE2D(Exp_Basic):
             channel_idx = list(range(data.shape[1]))
 
         segments = []
-        for start_idx in range(0, data.data.shape[0], self.recon_len):
-            end_idx = start_idx + self.recon_len
+        for start_idx in range(0, data.data.shape[0], self.args.recon_len):
+            end_idx = start_idx + self.args.recon_len
             if end_idx < data.shape[0]:
                 segments.append(data[start_idx : end_idx])
 
@@ -333,4 +391,6 @@ class Exp_VAE2D(Exp_Basic):
             
         plt.tight_layout()
         plt.savefig(file_path)
+        self.logger.log(f'saved reconstruction figures to {folder_path}',
+                        level='debug')
         plt.close(fig)
